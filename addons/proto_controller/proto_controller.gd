32f4947c97ad9ea525c720b3d1b2b216e52e9a7c
CozyCozmic <136282740+CozyCozmic@users.noreply.github.com>
2025-10-30 13:26:17 -0400

    Added Headbobbing to our player script. Also Added WASD input

--- addons/proto_controller/proto_controller.gd
+++ addons/proto_controller/proto_controller.gd
@@ -1,47 +1,37 @@
-# ProtoController v1.0 by Brackeys
+# ProtoController v1.1 by Brackeys + Headbobbing Addon by ChatGPT
 # CC0 License
 # Intended for rapid prototyping of first-person games.
-# Happy prototyping!
+
+#Modified by CozyCozmic to include headbobbing.
 
 extends CharacterBody3D
 
-## Can we move around?
+## === TOGGLES ===
 @export var can_move : bool = true
-## Are we affected by gravity?
 @export var has_gravity : bool = true
-## Can we press to jump?
 @export var can_jump : bool = true
-## Can we hold to run?
 @export var can_sprint : bool = false
-## Can we press to enter freefly mode (noclip)?
 @export var can_freefly : bool = false
+@export var headbob_enabled : bool = true  # 🟢 NEW: Toggle for headbobbing
 
 @export_group("Speeds")
-## Look around rotation speed.
 @export var look_speed : float = 0.002
-## Normal speed.
 @export var base_speed : float = 7.0
-## Speed of jump.
 @export var jump_velocity : float = 4.5
-## How fast do we run?
 @export var sprint_speed : float = 10.0
-## How fast do we freefly?
 @export var freefly_speed : float = 25.0
 
+@export_group("Headbob Settings")
+@export var bob_frequency : float = 7.0
+@export var bob_amplitude : float = 0.05
+
 @export_group("Input Actions")
-## Name of Input Action to move Left.
-@export var input_left : String = "ui_left"
-## Name of Input Action to move Right.
-@export var input_right : String = "ui_right"
-## Name of Input Action to move Forward.
-@export var input_forward : String = "ui_up"
-## Name of Input Action to move Backward.
-@export var input_back : String = "ui_down"
-## Name of Input Action to Jump.
-@export var input_jump : String = "ui_accept"
-## Name of Input Action to Sprint.
+@export var input_left : String = "move_left"
+@export var input_right : String = "move_right"
+@export var input_forward : String = "move_forward"
+@export var input_back : String = "move_backward"
+@export var input_jump : String = "jump"
 @export var input_sprint : String = "sprint"
-## Name of Input Action to toggle freefly mode.
 @export var input_freefly : String = "freefly"
 
 var mouse_captured : bool = false
@@ -49,7 +39,11 @@ var look_rotation : Vector2
 var move_speed : float = 0.0
 var freeflying : bool = false
 
-## IMPORTANT REFERENCES
+# 🟢 Headbob control
+var bob_time : float = 0.0
+var head_start_position : Vector3
+
+## REFERENCES
 @onready var head: Node3D = $Head
 @onready var collider: CollisionShape3D = $Collider
 
@@ -57,19 +51,17 @@ func _ready() -> void:
 	check_input_mappings()
 	look_rotation.y = rotation.y
 	look_rotation.x = head.rotation.x
+	head_start_position = head.position  # 🟢 store starting head pos
 
 func _unhandled_input(event: InputEvent) -> void:
-	# Mouse capturing
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		capture_mouse()
 	if Input.is_key_pressed(KEY_ESCAPE):
 		release_mouse()
 	
-	# Look around
 	if mouse_captured and event is InputEventMouseMotion:
 		rotate_look(event.relative)
 	
-	# Toggle freefly mode
 	if can_freefly and Input.is_action_just_pressed(input_freefly):
 		if not freeflying:
 			enable_freefly()
@@ -77,7 +69,6 @@ func _unhandled_input(event: InputEvent) -> void:
 			disable_freefly()
 
 func _physics_process(delta: float) -> void:
-	# If freeflying, handle freefly and nothing else
 	if can_freefly and freeflying:
 		var input_dir := Input.get_vector(input_left, input_right, input_forward, input_back)
 		var motion := (head.global_basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
@@ -85,23 +76,18 @@ func _physics_process(delta: float) -> void:
 		move_and_collide(motion)
 		return
 	
-	# Apply gravity to velocity
 	if has_gravity:
 		if not is_on_floor():
 			velocity += get_gravity() * delta
 
-	# Apply jumping
-	if can_jump:
-		if Input.is_action_just_pressed(input_jump) and is_on_floor():
-			velocity.y = jump_velocity
+	if can_jump and Input.is_action_just_pressed(input_jump) and is_on_floor():
+		velocity.y = jump_velocity
 
-	# Modify speed based on sprinting
 	if can_sprint and Input.is_action_pressed(input_sprint):
-			move_speed = sprint_speed
+		move_speed = sprint_speed
 	else:
 		move_speed = base_speed
 
-	# Apply desired movement to velocity
 	if can_move:
 		var input_dir := Input.get_vector(input_left, input_right, input_forward, input_back)
 		var move_dir := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
@@ -115,13 +101,12 @@ func _physics_process(delta: float) -> void:
 		velocity.x = 0
 		velocity.y = 0
 	
-	# Use velocity to actually move
 	move_and_slide()
+	
+	# 🟢 Apply headbobbing each frame
+	if headbob_enabled:
+		apply_headbob(delta)
 
-
-## Rotate us to look around.
-## Base of controller rotates around y (left/right). Head rotates around x (up/down).
-## Modifies look_rotation based on rot_input, then resets basis and rotates by look_rotation.
 func rotate_look(rot_input : Vector2):
 	look_rotation.x -= rot_input.y * look_speed
 	look_rotation.x = clamp(look_rotation.x, deg_to_rad(-85), deg_to_rad(85))
@@ -130,7 +115,6 @@ func rotate_look(rot_input : Vector2):
 	rotate_y(look_rotation.y)
 	head.transform.basis = Basis()
 	head.rotate_x(look_rotation.x)
-
 
 func enable_freefly():
 	collider.disabled = true
@@ -141,19 +125,14 @@ func disable_freefly():
 	collider.disabled = false
 	freeflying = false
 
-
 func capture_mouse():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	mouse_captured = true
-
 
 func release_mouse():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	mouse_captured = false
 
-
-## Checks if some Input Actions haven't been created.
-## Disables functionality accordingly.
 func check_input_mappings():
 	if can_move and not InputMap.has_action(input_left):
 		push_error("Movement disabled. No InputAction found for input_left: " + input_left)
@@ -176,3 +155,14 @@ func check_input_mappings():
 	if can_freefly and not InputMap.has_action(input_freefly):
 		push_error("Freefly disabled. No InputAction found for input_freefly: " + input_freefly)
 		can_freefly = false
+
+# 🟢 Simple but natural headbob effect
+func apply_headbob(delta: float) -> void:
+	var horizontal_velocity = Vector3(velocity.x, 0, velocity.z).length()
+	if horizontal_velocity > 0.1 and is_on_floor():
+		bob_time += delta * bob_frequency * (horizontal_velocity / sprint_speed)
+		var bob_offset = sin(bob_time * TAU) * bob_amplitude
+		head.position.y = head_start_position.y + bob_offset
+	else:
+		# smoothly return to resting position
+		head.position.y = lerp(head.position.y, head_start_position.y, delta * 5)
